491a5ff1647eb8fb2e35cad0ff9be67943f8b63e
andrr64 <derzandrr03@gmail.com>
2023-11-22 22:02:02 +0700

    menambahkan top_bar.js

---
+++ src/script/top_bar.js
@@ -0,0 +1,32 @@
+function getListMenu(activeTopBar) {
+    var listMenu = ["Home", "Mobil", "Pelayanan", "Tentang", "Hubungi"];
+    var menuListHTML = "";
+
+    for (var i = 0; i < listMenu.length; i++) {
+        if (listMenu[i] === activeTopBar) {
+            menuListHTML += `<li><a class="active" href="../${listMenu[i]}/index.html">${listMenu[i]}</a></li>`;
+        } else {
+            menuListHTML += `<li><a href="../${listMenu[i]}/index.html">${listMenu[i]}</a></li>`;
+        }
+    }
+
+    return menuListHTML;
+}
+
+function getTopBar(activeTopBar) {
+    var menuList = getListMenu(activeTopBar);
+
+    var htmlCode = `
+    <nav>
+        <label class="logo">
+            <img src="../logo.png" alt="">
+        </label>
+        <ul>
+            ${menuList}
+        </ul>
+    </nav>
+    `;
+
+    return htmlCode;
+}
+
