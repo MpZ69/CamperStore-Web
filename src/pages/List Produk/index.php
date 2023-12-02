b838c9b4d1e8771ff52d0b6a934a78d3c19e5e62
andrr64 <derzandrr03@gmail.com>
2023-12-02 21:19:57 +0700

    update index.php

--- src/pages/List Produk/index.php
+++ src/pages/List Produk/index.php
@@ -16,51 +16,133 @@
     </div>
 
     <div class="content">
+        <div class="list_sepatu">
+            <table>
+                <!-- SEPATU GUNUNG - 1-->
+                <tr>
+                    <td>
+                        <div class="category">
+                            Sepatu Gunung
+                        </div>
+                    </td>
+                    
+                </tr>
+                <tr>
+                    <td class="product1x1" style="padding-right: 75px;padding-top: 20px">
+                        <script>
+                            document.querySelector(".product1x1").innerHTML = getViewProduct(
+                                "data/sepatu/hipzo-sepatu-gunung-kerja/hipzo-sepatung-gunung-kerja.jpg",
+                                "Sepatu Pria Hiking Casual Hipzo Sepatu Gunung Kerja Riding Terbaru - Navy 41",
+                                135000,
+                                `Hipzo Original Produk Kebanggaan anak bangsa dengan Kualitas International Model dan design Elegan dan Kekinian. Bahan Pilihan, Kualitas terjaga .
+
+                                Spesifikasi Produk :
+                                - Sepatu Hipzo M 032
+                                - Ready Size 39.40.41.42.43
+                                - Upper Webbing Tebal dan Kuat
+                                - Insole Karet Sponge Lentur & Nyaman
+
+                                Size Chart ;
+                                39 = 24.5 cm
+                                40 = 25 cm
+                                41 = 26 cm
+                                42 = 26.5 cm
+                                43 = 27.5 cm`
+                            );
+                        </script>
+                    </td>
+                    <td class="product1x2" style="padding-right: 75px;padding-top: 20px">
+                        <script>
+                            document.querySelector(".product1x2").innerHTML = getViewProduct(
+                                "data/sepatu/kanky-kitadake-gunung-casual/kanky-story-kitadake-gunung-casual-sportstyle.jpg",
+                                "Kanky Story Kitadake - Sepatu Gunung Casual SportStyle Pria - Green L. Green, 39",
+                                388800,
+                                `Kanky sportstyle is the new line from Sepatu Kanky that bring the outdoor looks but still great for daily use
+
+                                Coming with fresh ideas that bring you an outdoor shoes concept with a little touch of casual sports style, you can feel the balance living in the newest silhouette from Kanky sportstyle
+
+                                Made from high quality materials to support durability & flexibility of the shoes
+
+                                Specifications
+
+                                * Genuine leather suede on upper details
+                                * High quality mesh with great air flow system
+                                * Midsole phylon
+                                * 100% Rubber outsole
+                                * Eva Foam Insole
+                                * Cementing construction to attach the outsole and midsole to the upper body
+                                * Titan buckle lace
+                                * 3M webbing`
+                            );
+                        </script>
+                    </td>
+                    <td class="product1x3" style="padding-right: 75px;padding-top: 20px">
+                        <script>
+                            document.querySelector(".product1x3").innerHTML = getViewProduct(
+                                "data/sepatu/lavio-elektra-sepatu-hiking/lavio-elektra-sepatu-hiking.jpg",
+                                "Lavio Elektra Sepatu Riding Sepatu Proyek Sepatu Hiking Sepatu Safety - Cream 39",
+                                229000,
+                                `Lavio Boots model terbaru dan terlaris, sepatu yang cocok untuk anda yang senang dengan fashion update terbaru. Gambar diatas adalah Gambar Asli Dari Kita, dijamin real Picture, garansi kembali jika tidak sesuai.
+
+                                Brand : Lavio
+                                Finishing : Bagus, Rapih, Kuat dan Nyaman dipakai
+                                Material UP : Leather PU+Suede
+                                Material SOLE : Rubber
+                                Front : Terdapat Ujung Besi didepannya
+                                Ukuran Tersedia : 39-43
+                                Ukuran Jumbo 44-48 PO / Inden 6  hari
+                                Jahit Keililing`
+                            );
+                        </script>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="product1x4" style="padding-right: 75px;padding-top: 20px">
+                        <script>
+                            document.querySelector(".product1x4").innerHTML = getViewProduct(
+                                "data/sepatu/leedo-sepatu-gunung-MH103/leedo-sepatu-gunung-MH103.png",
+                                "Leedoo Sepatu Hiking Pria Casual Mendaki Sepatu Gunung Outdoor MH103",
+                                129000,
+                                `Sepatu sangat ringan dipakai sepatu dengan kualitas bagus dan kokoh, sepatu Hiking ini sepatu yg dipacking dengan box denagan kualitas unggulan dibuat menggunakan bahan lembut yang mengikuti lekuk kaki tidak keras sangat elegan dan cocok buat teman teman memberikan kesan modis casual dan trendy sangat berkualitas.
+
+                                SPESIFIKASI PRODUK :
+                                Bahan : PU + Jala
+                                Warna : Hitam dan Abu-abu
+                                Fitur : Anti Slip Sol Rubbernya membuatnya tidak licin
+                                Cocok Digunakan Untuk : Kuliah, Kerja, Traveling, Hiking, Touring
+                                Cocok Digunakan Dicuaca : Musim Hujan & Panas`
+                            );
+                        </script>
+                    </td>
+
+                    <td class="product1x5" style="padding-right: 75px;padding-top: 20px">
+                        <script>
+                            document.querySelector(".product1x5").innerHTML = getViewProduct(
+                                "data/sepatu/leedo-sepatu-gunung-MH208/leedo-sepatu-gunung-MH208.jpg",
+                                "Leedoo Sepatu Hiking Anti Air Sepatu Olahraga Sepatu Gunung MH208 - Abu-abu 39",
+                                499000,
+                                `Leedoo Sepatu Hiking Anti Air Sepatu Olahraga Sepatu Gunung MH208
+
+                                BONUS : FREE BOX & KAOS KAKI
+                                Sepatu ini di lengkapi alas yang sangat nyaman, desain yang elegan. sangat cocok digunakan di berbagai medan baik di medan berbatu, medan lantai, ataupun medan lumpur. Karena desain bahan menyesuaikan penggunaan
+
+                                Material : Rubber, PU Leather
+                                Warna : Abu-abu dan Hitam
+                                Ready Size
+                                40=25cm
+                                41=25.5cm
+                                42=26cm
+                                43=26.5cm
+                                44=27cm`
+                            );
+                        </script>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        
+        <div class="list_tas">
         <table>
-            <!-- SEPATU GUNUNG - 1-->
-            <tr>
-                <td>
-                    <div class="category">
-                        Sepatu Gunung
-                    </div>
-                </td>
-                
-            </tr>
-            <tr>
-                <td class="product1x1" style="padding-right: 20px;">
-                    <script>
-                        document.querySelector(".product1x1").innerHTML = getViewProduct(
-                            "data/sepatu/hipzo-sepatu-gunung-kerja/hipzo-sepatung-gunung-kerja.jpg",
-                            "Sepatu Pria Hiking Casual Hipzo Sepatu Gunung Kerja Riding Terbaru - Navy 41",
-                            "135.000",
-                            `Hipzo Original Produk Kebanggaan anak bangsa dengan Kualitas International Model dan design Elegan dan Kekinian. Bahan Pilihan, Kualitas terjaga .
-
-                            Spesifikasi Produk :
-                            - Sepatu Hipzo M 032
-                            - Ready Size 39.40.41.42.43
-                            - Upper Webbing Tebal dan Kuat
-                            - Insole Karet Sponge Lentur & Nyaman
-
-                            Size Chart ;
-                            39 = 24.5 cm
-                            40 = 25 cm
-                            41 = 26 cm
-                            42 = 26.5 cm
-                            43 = 27.5 cm`
-                        );
-                    </script>
-                </td>
-                <td class="product1x2">
-                    <script>
-                        document.querySelector(".product1x2").innerHTML = getViewProduct(
-                            "data/sepatu/kanky-kitadake-gunung-casual/kanky-story-kitadake-gunung-casual-sportstyle.jpg",
-                            "Kanky Story Kitadake - Sepatu Gunung Casual SportStyle Pria - Green L. Green, 39",
-                            "388.800"
-                        );
-                    </script>
-                </td>
-            </tr>
-
             <!-- TAS GUNUNG - 2 -->
             <tr>
                 <td>
@@ -70,12 +152,12 @@
                 </td>
             </tr>
             <tr>
-                <td class="product2x1" style="padding-right: 20px;">
+                <td class="product2x1" style="padding-right: 75px;padding-top: 20px">
                     <script>
                         document.querySelector(".product2x1").innerHTML = getViewProduct(
                             "data/tas/carrier-nature-hike-70l/carrier-nature-hike-70l.jpg",
                             "Carrier Naturehike 70L Tas Gunung Keril Hiking Kerir Outdoor Pendaki - Brown",
-                            "1.146.000",
+                            1146000,
                             `Hipzo Original Produk Kebanggaan anak bangsa dengan Kualitas International Model dan design Elegan dan Kekinian. Bahan Pilihan, Kualitas terjaga .
                             
                             Spesifikasi Produk :
@@ -93,18 +175,79 @@
                         );
                     </script>
                 </td>
-                <td class="product2x2" style="padding-right: 20px;">
+                <td class="product2x2" style="padding-right: 75px;padding-top: 20px">
                     <script>
                         document.querySelector(".product2x2").innerHTML = getViewProduct(
                             "data/tas/eiger-rhinos-45-ng-carrier/eiger-rhinos-45-ng-carrier.webp",
                             "Eiger Rhinos 45 NG Carrier - Gacor Kang Edition",
-                            "1.139.050"
+                            1139050,
+                            `
+                            Rhinos 45 NG adalah carrier yang dirancang untuk kegiatan hiking selama 1-3 hari. Ransel berkapasitas 45 Liter ini dilengkapi dengan fitur backsystem Fit Light yang dapat memberikan kenyamanan dan meminimalisir panas pada punggung Anda. Tas ini memiliki kompartemen utama untuk menyimpan dan menjangkau peralatan Anda dengan leluasa. Terdapat saku samping untuk menyimpan botol minum dan saku hipbelt untuk memudahkan penyimpanan barang bawaan yang sering diakses. Fitur tambahan lainnya yang mendukung kegiatan hiking Anda, diantaranya fitur tali dada, pengikat trekking pole, dan tali kompresi, dan lubang hydration system. Terdapat pula rain cover untuk memastikan barang bawaan Anda terlindungi saat hujan.
+
+                            Dimension: 30 x 25 x 67 cm (Vol 45L)
+                            Material:
+                            Polyester 600D
+                            Polyester 200D Recycled Lining
+
+                            Fitur:
+                            * Teknologi backsystem Fit Light.
+                            * Kompartemen utama.
+                            * Kompartemen depan.
+                            * Saku tutup atas.
+                            * Dua saku samping.
+                            * Tali kompresi samping.
+                            * Saku hipbelt.
+                            * Pengikat trekking pole.
+                            * Rain cover.`
+                        );
+                    </script>
+                </td>
+                <td class="product2x3" style="padding-right: 75px;padding-top: 20px">
+                    <script>
+                        document.querySelector(".product2x3").innerHTML = getViewProduct(
+                            "data/tas/eiger-z-safeguard-laptop-sleeve-2f/tas-laptop-z-safeguard-laptop-sleeve-2f.webp",
+                            "Eiger Z-Safeguard Laptop Sleefe 2F",
+                            279000,
+                            `
+                            Z-Safeguard Laptop Sleeve 2F adalah tas yang didesain ramping dan praktis digunakan untuk menyimpan laptop Anda tetap rapi, mudah diakses, dan aman saat berkegiatan sehari-hari. Kompartemen utama tas ini dapat memuat laptop 15”. Saku-saku di bagian depan dapat memuat gadget seperti kabel power charger laptop, mouse, ballpoint, ponsel, harddisk, powerbank, kabel-kabel, dll.
+
+                            Dimension: 28 x 8 x 42,5 cm
+
+                            Fitur:
+                            * Kompartemen utama yang dapat memuat laptop 15”.
+                            * Tiga saku di bagian depan untuk membawa perlengkapan gadget seperti kabel power charger laptop, ponsel, harddisk, powerbank, kabel-kabel, dll.
+                            * Webbing loop dengan reflektif aksen.`
                         );
                     </script>
                 </td>
             </tr>
-
+            <tr>
+                <td class="product2x4" style="padding-right: 75px;padding-top: 20px">
+                    <script>
+                        document.querySelector(".product2x4").innerHTML = getViewProduct(
+                            "data/tas/techdoo-tas-ransel-40L-TR109/techdoo-tas-ransel-40L-TR109.jpg",
+                            "Techdoo Tas Ransel Gunung 40L Backpack Anti Air Hiking Camping TR109 - Biru",
+                            108000,
+                            `Hipzo Original Produk Kebanggaan anak bangsa dengan Kualitas International Model dan design Elegan dan Kekinian. Bahan Pilihan, Kualitas terjaga .
+                            
+                            Spesifikasi Produk :
+                            - Sepatu Hipzo M 032
+                            - Ready Size 39.40.41.42.43
+                            - Upper Webbing Tebal dan Kuat
+                            - Insole Karet Sponge Lentur & Nyaman
+                            
+                            Size Chart ;
+                            39 = 24.5 cm
+                            40 = 25 cm
+                            41 = 26 cm
+                            42 = 26.5 cm
+                            43 = 27.5 cm`
+                        );
+                    </script>
+                </td>
+            </tr>
         </table>
+        </div>
     </div>
 </body>
 </html>
